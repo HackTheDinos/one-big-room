9d4c97b4944783d2ca405a3616c6aca5ccdc2c2a
Amy Ciavolino <amyciav@gmail.com>
2015-11-21 18:52:28 -0500

    Fix tpl file path

--- bone_explorer/views.py
+++ bone_explorer/views.py
@@ -3,7 +3,7 @@ import pystache
 import os
 
 def mustache_render(tpl_file, data):
-    return pystache.render(open('templates/' + tpl_file, 'r').read(), data)
+    return pystache.render(open('bone_explorer/templates/' + tpl_file, 'r').read(), data)
 
 @app.route('/')
 def index():
