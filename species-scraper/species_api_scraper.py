d0eb96ec56c1387ebb75d84bb228b01e7d8af943
amandapickering <pickering.amanda@gmail.com>
2015-11-22 11:59:04 -0500

    fixed species api scraper

--- species-scraper/species_api_scraper.py
+++ species-scraper/species_api_scraper.py
@@ -2,30 +2,24 @@
 
 import requests
 import argparse
+import json
 
 BASE = "http://api.gbif.org/v1/species/search"
 # SPECIES = ["Alioramus", "Melanerpes", "Zanabazar"]
 
-def make_urls(base, param_list):
-	url_list = []
+def get_species_data(base, param_list, species_group):
 	for p in param_list:
 		payload = {"q": p}
-		r = requests.get(BASE, params=payload)
-		url_list.append(r.url)
-	return url_list
-
-def hit_the_api(url_list):
-	json_mega = []
-	for u in url_list:
 		try:
-			json_mega.append(requests.get(u).json()["results"][0])
+			r = requests.get(BASE, params=payload).json()["results"][0]
+			with open(species_group + ".json", "a") as f:
+				f.write(json.dumps(r))
+				f.write("\n")
 		except IndexError:
 			pass
-	return json_mega
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("params", nargs="+")
 	args = parser.parse_args()
-	urls = make_urls(BASE, args.params)
-	hit_the_api(urls)
+	get_species_data(BASE, params)
