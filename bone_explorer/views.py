142d46374d704600eb6a04d86b57c2db56827218
Gabrielle Gianelli <ggianelli@etsy.com>
2015-11-21 22:16:30 -0500

    adding to the search results page

--- bone_explorer/views.py
+++ bone_explorer/views.py
@@ -4,6 +4,18 @@ import os
 
 pystache.defaults.SEARCH_DIRS.append("./bone_explorer/static/templates")
 
+mallardData = {  
+  'imageUrl':'http://digimorph.org/specimens/anas_platyrhynchos/skull/specimen.jpg',
+  'scientificName':'Anas platyrhynchos',
+  'commonName': 'Domestic Mallard',
+  'addedBy': 'Richard Ketcham',
+  'addedFor': 'David Dufeau and Timothy Rowe',
+  'addedByDate': '5/12/98',
+  'classification' : ['Aves', 'Anseriformes', 'Anatidae'],
+  'wikipedia': 'The mallard or wild duck (Anas platyrhynchos) is a dabbling duck which breeds throughout the temperate and subtropical Americas, Europe, Asia, and North Africa, and has been introduced to New Zealand, Australia, Peru, Brazil, Uruguay, Argentina, Chile, the Falkland Islands and South Africa.[2] This duck belongs to the subfamily Anatinae of the waterfowl family'
+}
+
+
 def mustache_render(tpl_file, data):
     return pystache.render(open('bone_explorer/static/templates/' + tpl_file, 'r').read(), data)
 
@@ -14,26 +26,26 @@ def index():
 @app.route('/specimen')
 def specimen():
     pageTitle = 'Specimen Page'
-    data = { 
 
-            'pageTitle': pageTitle,
-            'imageUrl':'http://digimorph.org/specimens/anas_platyrhynchos/skull/specimen.jpg',
-            'scientificName':'Anas platyrhynchos',
-            'commonName': 'Domestic Mallard',
-            'addedBy': 'Richard Ketcham',
-            'addedFor': 'David Dufeau and Timothy Rowe',
-            'addedByDate': '5/12/98',
-            'classification' : ['Aves', 'Anseriformes', 'Anatidae']
-        }
+    data = {}
+    data['pageTitle'] = pageTitle
+    data.update(mallardData)
+
     return mustache_render('specimen.mustache', data)
 
 @app.route('/search')
 def specimenSearch():
-    #render the search page
+    pageTitle = 'Search for Specimens'
 
     return mustache_render('search.mustache', { 'message': 'Hello!'})
 
 @app.route('/results')
 def results():
     #render the search results page
+    pageTitle = 'Search Results'
+
+    data = {
+       'resultsList' : [ mallardData ],
+       'pageTitle' : pageTitle
+    }
     return mustache_render('results.mustache', { 'message': 'Hello!'})
