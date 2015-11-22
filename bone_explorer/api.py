c325d6859e8d8be1a890da189e8e2ca11008fafc
Fiona Condon <frcondon@gmail.com>
2015-11-21 22:50:13 -0500

    Adding ES search integration

--- bone_explorer/api.py
+++ bone_explorer/api.py
@@ -1,5 +1,6 @@
 from bone_explorer import app
 from flask import request, jsonify
+from search import test
 
 API_URL = '/api'
 
@@ -7,25 +8,6 @@ API_URL = '/api'
 def search():
     query = request.args.get('q')
     return jsonify({
-        'query': query,
-        'results': [
-            {
-                'kingdom':  'Animalia',
-                'phylum':   'Chordata',
-                'order':    'Saurischia',
-                'suborder': 'Theropoda',
-                'family':   'Tyrannosauridae',
-                'tribe':    'Alioramini',
-                'genus':    'Alioramus'
-            },
-            {
-                'kingdom':  'Animalia 2',
-                'phylum':   'Chordata 2',
-                'order':    'Saurischia 2',
-                'suborder': 'Theropoda 2',
-                'family':   'Tyrannosauridae 2',
-                'tribe':    'Alioramini 2',
-                'genus':    'Alioramus 2'
-            }
-        ]
+        'q': query,
+        'results': test()
     })
