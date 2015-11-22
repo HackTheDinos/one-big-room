27dbb82185a25563265a0a5ab7b9e415614cafd0
amandapickering <pickering.amanda@gmail.com>
2015-11-22 12:37:35 -0500

    rest of urls for digimorph species

---
+++ species-scraper/species_list_from_digimorph.py
@@ -0,0 +1,16 @@
+#!usr/bin/env pytjon
+
+import requests
+from bs4 import BeautifulSoup
+
+def get_species_names_from_digimorph(digimorph_url):
+    r = requests.get(digimorph_url)
+    soup = BeautifulSoup(r.text)
+    dinos_hopefully = soup('table')[5].findAll("a")
+    grouped = [dinos_hopefully[i:i+3] for i in xrange(0, len(dinos_hopefully), 3)] 
+    chopped = grouped[8:]
+    data = []
+    for c in chopped:
+        name = c[1].text.strip()
+        data.append(name)
+    return list(set(data))
