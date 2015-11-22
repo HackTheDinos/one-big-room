d2a9cf0ae2e7fac2670ce89e0fb7150ec783c3e7
amandapickering <pickering.amanda@gmail.com>
2015-11-21 23:47:43 -0500

    scrapes digimorph html tables

--- species-scraper/digimorph_scraper.py
+++ species-scraper/digimorph_scraper.py
@@ -5,14 +5,28 @@ import requests
 from bs4 import BeautifulSoup
 
 digimorph = "http://digimorph.org/listbygroup.phtml?grp=dinosaur&sort=CommonName" # test for below
+base = "http://digimorph.org/specimens"
 
 def scrape_digimorph_html_tables(digimorph_url):
 	'''gets the list of species from a digimorph
-	browse page, super hacky sry'''
+	browse page, super hacky and bad sry'''
 	r = requests.get(digimorph_url)
 	soup = BeautifulSoup(r.text)
-	dinos_hopefully = soup('table')[5].findAll("a").text
+	dinos_hopefully = soup('table')[5].findAll("a")
 	grouped = [dinos_hopefully[i:i+3] for i in xrange(0, len(dinos_hopefully), 3)] 
 	chopped = grouped[8:]
-	return json.dumps(chopped)
-
+	final = []
+	for c in chopped:
+		for a in c:
+			if a.get("name"):
+				final.append(a.get("name"))
+			if a.get("href"):
+				final.append(base + a.get("href"))
+			if a.text:
+				final.append(a.text)
+	regrouped = [final[i:i+3] for i in xrange(0, len(final), 3)] # could probably not group twice but ??!?
+	keys = ["group", "link", "name"]
+	actual_final = []
+	for g in regrouped:
+		actual_final.append(dict(zip(keys, g)))
+	return json.dumps(actual_final)
